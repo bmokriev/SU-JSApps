3deb7891d316ea8f1714f8193be26df01650fd06
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-10 19:20:50 +0200

    ex

--- Asynchronous-Programming/Lab/02.Github-Repos/scripts/app.js
+++ Asynchronous-Programming/Lab/02.Github-Repos/scripts/app.js
@@ -1,3 +1,20 @@
 function loadRepos() {
-	console.log("TODO...");
+	const username = document.querySelector('#username');
+	const url = `https://api.github.com/users/${username.value}/repos`;
+
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			const outputEl = document.querySelector('#repos');
+			outputEl.innerHTML = '';
+			data.forEach((repo) => {
+				const aEl = document.createElement('a');
+				const liEl = document.createElement('li');
+				aEl.setAttribute('href', repo.html_url);
+				aEl.setAttribute('target', '_blank')
+				aEl.textContent = repo.full_name;
+				liEl.appendChild(aEl)
+				outputEl.appendChild(liEl);
+			});
+		});
 }
