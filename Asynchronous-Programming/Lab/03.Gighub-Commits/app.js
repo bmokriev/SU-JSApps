ab4f792e869cc0b642eb1c89dba0d10cc502f3e8
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-13 18:40:44 +0200

    ex

--- Asynchronous-Programming/Lab/03.Gighub-Commits/app.js
+++ Asynchronous-Programming/Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username = document.querySelector('#username').value;
+    const repo = document.querySelector('#repo').value;
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`
+
+    getRepos(url);
+    async function getRepos(url) {
+        const ulEl = document.getElementById('commits');
+        try {
+            const res = await fetch(url);
+            if (res.ok === false) {
+                throw new Error(res.status)
+            }
+            const data = await res.json();
+            ulEl.innerHTML = '';
+            data.forEach(r => {
+                const liEl = document.createElement('li');
+                liEl.textContent = `${r.commit.author.name}: ${r.commit.message}`;
+                ulEl.appendChild(liEl);
+            });
+        } catch (error) {
+            ulEl.innerHTML = '';
+            const liEl = document.createElement('li');
+            liEl.textContent = `${error} (Not Found)`;
+            ulEl.appendChild(liEl);
+        }
+    }
 }
