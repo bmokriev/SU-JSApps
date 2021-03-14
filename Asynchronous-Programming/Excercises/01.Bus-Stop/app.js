8110cd2e5f77ac8b93e10e69ebdf94bb7c901e89
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-14 13:04:53 +0200

    ex

--- Asynchronous-Programming/Excercises/01.Bus-Stop/app.js
+++ Asynchronous-Programming/Excercises/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
-function getInfo() {
-    console.log("TODO...");
+const stopName = document.querySelector('#stopName');
+const ulElBuses = document.querySelector('#buses');
+
+async function getInfo() {
+    const stopId = document.querySelector('#stopId').value
+    const url = 'http://localhost:3030/jsonstore/bus/businfo/' + stopId;
+    try {
+        ulElBuses.innerHTML = '';
+        let res = await fetch(url);
+        let data = await res.json();
+        displayBusStop(data.name)
+        displayBuses(data.buses)
+    } catch (error) {
+        stopName.textContent = 'Error'
+    }
+}
+
+function displayBusStop(name) {
+    stopName.textContent = name;
+}
+
+function displayBuses(busObj) {
+    Object.entries(busObj).map(([bus, time]) => {
+        const liEl = document.createElement('li');
+        liEl.textContent = `Bus ${bus} arrives in ${time}`;
+        ulElBuses.appendChild(liEl);
+    })
 }
