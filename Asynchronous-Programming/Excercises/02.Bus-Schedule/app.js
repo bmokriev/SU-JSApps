6402ff849df3ed2a9afec4cb794bbd84e0f0f803
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-14 20:13:08 +0200

    ex

--- Asynchronous-Programming/Excercises/02.Bus-Schedule/app.js
+++ Asynchronous-Programming/Excercises/02.Bus-Schedule/app.js
@@ -1,11 +1,34 @@
 function solve() {
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
+    const infoBox = document.querySelector('.info');
+    let stop = {
+        next: 'depot'
+    }
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        try {
+            const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stop.next;
+
+            const res = await fetch(url);
+            const data = await res.json()
+            stop = data;
+
+            infoBox.textContent = `Next stop ${data.name}`;
+
+            arriveBtn.disabled = false;
+            departBtn.disabled = true;
+        } catch (err) {
+            infoBox.textContent = 'Error'
+            console.log(err);
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoBox.textContent = `Arriving at ${stop.name}`;
+
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
