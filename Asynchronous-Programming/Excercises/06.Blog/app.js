19bdf47073385c77ffe6db188eee356ed3e52a49
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-03-14 22:17:08 +0200

    fetching functions

--- Asynchronous-Programming/Excercises/06.Blog/app.js
+++ Asynchronous-Programming/Excercises/06.Blog/app.js
@@ -1,5 +1,27 @@
+const btnLoadPosts = document.querySelector('#btnLoadPosts');
+const postsSelectEl = document.querySelector('#posts');
+
+
 function attachEvents() {
     console.log('TODO...');
 }
 
 attachEvents();
+
+async function getPosts() {
+    const url = 'http://localhost:3030/jsonstore/blog/posts';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+};
+
+async function getCommnetsByPostId(postId) {
+    const url = 'http://localhost:3030/jsonstore/blog/comments';
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return Object.values(data).filter(e => e.postId === postId);
+};
