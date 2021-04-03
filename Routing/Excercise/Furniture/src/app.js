78f7c4613c7dd3e1273a51163bc289cb707a878c
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-04-03 11:39:46 +0300

    Creating needed js files
    Creating needed js files

---
+++ Routing/Excercise/Furniture/src/app.js
@@ -0,0 +1,10 @@
+import page from '../node_modules/page/page.js';
+
+page('/');
+page('/dahboard');
+page('/details/:id');
+page('/create');
+page('/edit');
+page('/register');
+page('/login');
+page('/my-furniture');
