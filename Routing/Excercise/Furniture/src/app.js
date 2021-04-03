12bb7461b2b7cc2c69e449159b83e09d65d7e57c
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-04-03 18:55:37 +0300

    creating dashboard view

--- Routing/Excercise/Furniture/src/app.js
+++ Routing/Excercise/Furniture/src/app.js
@@ -1,4 +1,5 @@
 import page from '../node_modules/page/page.mjs';
+import { render } from '../node_modules/lit-html/lit-html.js'
 
 import { dashboardPage } from './views/dashboard.js';
 import { myPage } from './views/myFurniture.js';
@@ -11,13 +12,20 @@ import { registerPage } from './views/register.js';
 import * as api from './api/data.js';
 window.api = api;
 
-page('/', dashboardPage);
-page('/dahboard', dashboardPage);
-page('/my-furniture', myPage);
-page('/details/:id', detailsPage);
-page('/create', createPage);
-page('/edit', editPage);
-page('/register', registerPage);
-page('/login', loginPage);
+const main = document.querySelector('.container');
+
+page('/', renderMiddleware, dashboardPage);
+page('/dahboard', renderMiddleware, dashboardPage);
+page('/my-furniture', renderMiddleware, myPage);
+page('/details/:id', renderMiddleware, detailsPage);
+page('/create', renderMiddleware, createPage);
+page('/edit', renderMiddleware, editPage);
+page('/register', renderMiddleware, registerPage);
+page('/login', renderMiddleware, loginPage);
 
 page.start();
+
+function renderMiddleware(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
